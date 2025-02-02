b517367da2d699f2fd9e71a06ef2bd9245425468
Muhammad Azzazy <muhammad-azzazy@aucegypt.edu>
2025-02-02 09:53:01 +0200

    refactored the code a bit

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 from math import pi
 from sys import exit
 
-PI: str = str(pi)
 
-LIMIT: int = 15
+def main() -> None:
+    PI: str = str(pi)
 
-print(f'Welcome to The 🥧 Approximator!')
+    LIMIT: int = 15
+    print(f'Welcome to The 🥧 Approximator!')
 
-prompt: str = f'Enter the number of decimal places (up to {LIMIT}): '
+    prompt: str = f'Enter the number of decimal places (up to {LIMIT}): '
 
-while True:
-    user_input: str = input(prompt)
+    while True:
+        user_input: str = input(prompt)
+        if user_input == 'exit':
+            print('Thanks for trying my program!')
+            exit()
+        if not user_input.isnumeric() or int(user_input) > LIMIT:
+            print(f'Please enter a whole number between 0 and {LIMIT}...')
+            continue
 
-    if user_input == 'exit':
-        print('Thanks for trying my program!')
-        exit()
+        n: int = int(user_input)
+        print(f'pi to the {n}th decimal place is {PI[:n+2]}')
 
-    if not user_input.isnumeric() or int(user_input) > LIMIT:
-        print(f'Please enter a whole number between 0 and {LIMIT}...')
-        continue    
-    
-    n: int = int(user_input)
-    print(f'pi to the {n}th decimal place is {PI[:n+2]}')
+
+main()
