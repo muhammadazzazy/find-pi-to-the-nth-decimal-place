991f8269d328c2b2723efffb2ad4e52b29bf1f1c
Muhammad Azzazy <muhammad-azzazy@aucegypt.edu>
2025-02-14 21:05:37 +0200

    added functionality allowing number of decimal places to be entered as words

--- main.py
+++ main.py
@@ -1,24 +1,43 @@
 from math import pi
 from sys import exit
+from word2number import w2n
 
 
 def main() -> None:
     PI: str = str(pi)
     LIMIT: int = 15
     print(f'Welcome to The 🥧 Approximator!')
+    error_message: str = f'Please enter a whole number between 0 and {LIMIT}...'
+    exit_message: str = 'Exiting program...'
 
     while True:
-        user_input: str = input(
-            f'Enter the number of decimal places (up to {LIMIT}): ')
-        if user_input == 'exit':
-            print('Exiting program...')
-            exit()
-        if not user_input.isnumeric() or int(user_input) > LIMIT:
-            print(f'Please enter a whole number between 0 and {LIMIT}...')
-            continue
+        try:
+            user_input: str = input(
+                f'Enter the number of decimal places (up to {LIMIT}): ')
 
-        n: int = int(user_input)
-        print(f'pi to the {n}th decimal place is {PI[:n+2]}')
+            if user_input == 'exit':
+                print(exit_message)
+                exit()
+
+            if user_input.isnumeric() and int(user_input) <= LIMIT:
+                n: int = int(user_input)
+            elif not user_input.isnumeric():
+                n: int = w2n.word_to_num(user_input)
+                if n > LIMIT:
+                    print(error_message)
+                    continue
+            else:
+                print(error_message)
+                continue
+
+        except ValueError:
+            print(error_message)
+
+        except KeyboardInterrupt:
+            print(exit_message)
+            exit()
+
+        print(f'π = {PI[:n+2]} (to {n} d.p.)')
 
 
 if __name__ == '__main__':
